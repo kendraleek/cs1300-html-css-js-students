cc22f102f9fdf5b5de81a674532f205af1870e45
kendraleek <kendralee@kendras-air.devices.brown.edu>
2022-09-10 18:01:36 -0400

    done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -40,15 +40,26 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const extraSugar = fruitsArray.filter((fruit => 
+    fruit.nutritions.sugar > 15));
+    // console.log(extraSugar);
 
   // TODO: Create a new HTML element to display your data 
+    const sugarMap = extraSugar.map(fruit => 
+      fruit.name
+    )
+    const ele = document.createElement('div');
+    ele.innerText = sugarMap;
+    const parent = document.getElementById('add');
+    parent.append(ele);
+  
 
   // TODO: Append your new element to the page
-
+  
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
